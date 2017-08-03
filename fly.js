2fdb6d258f8702a26f1c68966f4349698979859c
Frank Cizmich <frank.cizmich@gmail.com>
2017-08-03 13:54:35 -0300

    Update fly.js

--- fly.js
+++ fly.js
@@ -1,8 +1,17 @@
 // Frank Cizmich  
-// No warranty of any kind. 
-// Free to use, modify, etc. 
-// If you keep my name up there I wouldn't mind. 
 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // here comes some base64 data (the fly image info) so watch out if your text editor can't handle it 
 
